5d9a92a6d73c5d07be1278b60aeef7a97b08d98a
sergio sacristan santero <srg_ek@hotmail.com>
2020-10-22 18:48:08 +0200

    [SpringBoot] - implementacion de encriptacion de contraseñas

--- src/main/java/com/springBoot/DemoApplication.java
+++ src/main/java/com/springBoot/DemoApplication.java
@@ -1,21 +1,22 @@
 package com.springBoot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.springBoot.service.MainService;
+import com.springBoot.dto.IUsuarioRepo;
+import com.springBoot.dto.Usuario;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner{
 	
-	private static Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
+	@Autowired
+	private IUsuarioRepo user;
 	
 	@Autowired
-	private MainService objPrincipal;
+	private BCryptPasswordEncoder encoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -23,7 +24,13 @@ public class DemoApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		Usuario us = new Usuario();
+		us.setId(3);
+		us.setNombre("sergioCody");
+		us.setClave(encoder.encode("sergio"));
+		
+		user.save(us);
+		
 	}
 
 }
